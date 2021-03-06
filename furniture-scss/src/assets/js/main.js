10ac02200ba6a86f77e9f38b9de87a48c6bfaec8
Todor Shishmanov <todor.shishmanoff@gmail.com>
2021-03-06 17:54:01 +0200

    begin

---
+++ furniture-scss/src/assets/js/main.js
@@ -0,0 +1,7 @@
+;(function(window, document, $) {
+	var $win = $(window);
+	var $doc = $(document);
+
+	// Your code goes here...
+	// jQuery.ready is no longer needed
+})(window, document, window.jQuery);
